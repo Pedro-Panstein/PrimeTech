364d940f70de7bd4cc8ca9c9e9cdc9fd3971face
Pedro Henrique Panstein <phfarof@gmail.com>
2024-08-15 21:50:53 -0300

    21:50 - 15/08/2024

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,24 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { HeaderComponent } from './components/header/header.component';
 import { MatIconModule } from '@angular/material/icon';
 import { HomeComponent } from './pages/home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule, 
     MatIconModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [
     provideClientHydration(),
